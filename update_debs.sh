9ecbbe9d01df2805f2792a91dd773258fd9a7939
xlsn0w <xlsn0wios@gmail.com>
2021-02-12 17:01:44 +0800

    add

---
+++ update_debs.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# 自动脚本执行 把本文件直接拖到终端 按Enter一步到位
+# $ /Users/xlsn0w/debs/update_debs.sh
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+cd $SCRIPTPATH
+
+# 手动终端执行
+# cd 根目录路径
+# cd /Users/xlsn0w/debs
+
+# 删除之前Packages和Packages.bz2
+rm ./Packages
+rm ./Packages.bz2
+
+# 创建新的Packages
+dpkg-scanpackages debs / > Packages
+
+# 创建新的Packages.bz2
+bzip2 -fks Packages
--- 删掉bz2重新生成bz2.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-dpkg-scanpackages -m ./debs > Packages
-rm -r -f /Users/tashigefengzi/Desktop/github/yilai/Packages.bz2
-bzip2 Packages
-dpkg-scanpackages -m ./debs > Packages
